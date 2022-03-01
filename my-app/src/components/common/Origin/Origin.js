0a91ba8d844a09f5e3951be8e5b03813ab8a4c16
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-03-01 17:41:47 +0400

    change div name

--- my-app/src/components/common/Origin/Origin.js
+++ my-app/src/components/common/Origin/Origin.js
@@ -5,12 +5,12 @@ function Origin () {
     return(
         <div className="OriginPage">
             <div className="leftSide">
-                <div className="Origin-heading">
+                <div className="OriginHeading">
                 <h2>Hey, Rocketeer, what are your coordinates?</h2>
                 </div>
             </div>
             <div className="rightSide">
-                
+                div.origini
             </div>
         </div>
     )
