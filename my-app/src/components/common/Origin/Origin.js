724c04ef5792cb3aed32c2467c3132b4037354ae
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-03-01 17:55:57 +0400

    add rightSide markup & styles

--- my-app/src/components/common/Origin/Origin.js
+++ my-app/src/components/common/Origin/Origin.js
@@ -10,7 +10,16 @@ function Origin () {
                 </div>
             </div>
             <div className="rightSide">
-                div.origini
+                <div className="originHeadingTwo">
+                    <h2>Redberry Origins</h2>
+                </div>
+                <div className="rightSideText">
+                    <p>You watch “What? Where? When?” Yeah. Our founders used to play it.
+                        That’s where they got a question about a famous American author and screenwriter Ray Bradbury.
+                        Albeit, our CEO Gaga Darsalia forgot the exact name and he answered Ray Redberry.
+                        And at that moment, a name for a yet to be born company was inspired - Redberry 😇
+                    </p>
+                </div>
             </div>
         </div>
     )
