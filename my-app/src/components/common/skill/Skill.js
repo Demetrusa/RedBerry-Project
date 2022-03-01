0eebe8846c3990cdb6a4c3a495831430e6312093
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-03-01 23:04:04 +0400

    add skill Section styles

--- my-app/src/components/common/skill/Skill.js
+++ my-app/src/components/common/skill/Skill.js
@@ -1,34 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Skill.css";
 
 function Skill () {
+    const [skills, setSkill] = useState("");
     const skillApi = () => {
-        
+        fetch("https://bootcamp-2022.devtest.ge/api/skills")
+        .then((response) => response.json())
+        .then((data)=>{
+            setSkill(data.setup + " ... " + data.title)
+        });
+
     }
     return (
         <div className="skill">
             <div className="abouTSkill">
-                <h2>Tell us about your skills</h2>
-                <select name="custom-select" id="skillChange" >
-                    <option value="HTML"></option>
-                    <option value="CSS"></option>
-                    <option value="PHP"></option>
-                    <option value="Laravel"></option>
-                    <option value="React.JS"></option>
-                    <option value="Vue.JS"></option>
-                    <option value="Svelte"></option>
-                    <option value="Angular"></option>
+                <div className="skillHeading">
+                    <h2>Tell us about your skills</h2>
+                </div>
+                <div className="skillForm">
+                <select name="custom-select" id="skillChange" placeholder="Skills" onClick={setSkill} >
+                    <option value="HTML">{skills}</option>
+                    <option value="CSS">{skills}</option>
+                    <option value="PHP">{skills}</option>
+                    <option value="Laravel">{skills}</option>
+                    <option value="React.JS">{skills}</option>
+                    <option value="Vue.JS">{skills}</option>
+                    <option value="Svelte">{skills}</option>
+                    <option value="Angular">{skills}</option>
                 </select>
+                </div>
             </div>
             <div className="AboutRedberry">
-                <h2>A bit about our battles</h2>
-                <p>
-                As we said, Redberry has been on the field for quite some time now,
-                and we have touched and embraced a variety of programming languages,
-                technologies, philosophies, and frameworks. We are battle-tested in PHP Laravel Stack with Vue.
-                js, refined in React, and allies with Serverside technologies like Docker and Kubernetes,
-                and now we have set foot in the web3 industry.
-                </p>
+                <div className="aboutreadberry-heading">
+                    <h2>A bit about our battles</h2>
+                </div>
+                <div className="AboutTextContent">
+                    <p>
+                    As we said, Redberry has been on the field for quite some time now,
+                    and we have touched and embraced a variety of programming languages,
+                    technologies, philosophies, and frameworks. We are battle-tested in PHP Laravel Stack with Vue.
+                    js, refined in React, and allies with Serverside technologies like Docker and Kubernetes,
+                    and now we have set foot in the web3 industry.
+                    </p>
+                </div>
             </div>
         </div>
     )
