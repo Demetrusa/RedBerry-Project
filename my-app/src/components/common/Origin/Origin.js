6598a0df4a3403e1d44b79a7f60187bfe0b08e5c
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-03-01 17:40:42 +0400

    change h2 position in div & add styles

--- my-app/src/components/common/Origin/Origin.js
+++ my-app/src/components/common/Origin/Origin.js
@@ -5,7 +5,9 @@ function Origin () {
     return(
         <div className="OriginPage">
             <div className="leftSide">
+                <div className="Origin-heading">
                 <h2>Hey, Rocketeer, what are your coordinates?</h2>
+                </div>
             </div>
             <div className="rightSide">
                 
