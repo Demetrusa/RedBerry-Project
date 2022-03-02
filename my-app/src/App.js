740b397cfe8b773030a7a07212dd36bd973d6579
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-03-03 00:10:52 +0400

    add fonts to About component & add padding

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,8 +15,8 @@ function App() {
       {/* <Header /> */}
       {/* <Origin /> */}
       {/* <Skill /> */}
-      <Covid />
-      {/* <About /> */}
+      {/* <Covid /> */}
+      <About />
       {/* <SubmitPage /> */}
       {/* <ThankYou /> */}
     
