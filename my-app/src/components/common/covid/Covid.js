ec4a7490496f702ee711fa71ab413f7fbaac3f89
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-03-02 20:19:17 +0400

    add CovidSection rightSide content ad styles

--- my-app/src/components/common/covid/Covid.js
+++ my-app/src/components/common/covid/Covid.js
@@ -41,7 +41,22 @@ function Covid() {
                     </div>
                 </div>
             </div>
-            <div className="rightCovidBox"></div>
+            <div className="rightCovidBox">
+                <div className="rightCovidBoxHeading">
+                    <h2>Redberry Covid Policies</h2>
+                </div>
+                <div className="rightCovidBoxText">
+                    <p>As this infamous pandemic took over the world, we
+                        adjusted our working practices so that our
+                        employees can be as safe and comfortable as
+                        possible. We have a hybrid work environment, so you
+                        can either work from home or visit our lovely office
+                        on Sairme Street. If it was up to us, we would love you
+                        to see us in the office because we think face-to-face 
+                        communications > Zoom meetings. Both on the fun 
+                        and productivity scales. </p>
+                </div>
+            </div>
         </div>
     )
 }
