047e94fa2a402353c4300b2a8796df9aaf3b7e39
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-03-02 23:59:50 +0400

    add Covid components font & removed padding

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,8 +14,8 @@ function App() {
     <div className="App">
       {/* <Header /> */}
       {/* <Origin /> */}
-      <Skill />
-      {/* <Covid /> */}
+      {/* <Skill /> */}
+      <Covid />
       {/* <About /> */}
       {/* <SubmitPage /> */}
       {/* <ThankYou /> */}
