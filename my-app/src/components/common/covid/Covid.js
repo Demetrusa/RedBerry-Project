12a008c36a9059a3a3ef1e81aaa4364828bfac29
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-03-02 19:56:10 +0400

    Covid leftbox markup & css add

--- my-app/src/components/common/covid/Covid.js
+++ my-app/src/components/common/covid/Covid.js
@@ -1,7 +1,13 @@
 import React from "react";
+// import BasicDatePicker from "./BasicDatePicker";
 import "./Covid.css";
 
+
+import "./Covid.css";
+
+
 function Covid() {
+
     return (
         <div className="covid-box">
             <div className="leftCovidBox">
@@ -11,23 +17,27 @@ function Covid() {
                 <div className="CovidRadio">
                     <div className="radioBox">
                         <h2>how would you prefer to work?</h2>
-                        <input type="radio"  id="Office" name="work" value="From Sairme Office"/>From Sairme Office<br/>
-                        <input type="radio"  id="Home" name="work" value="From Home"/>From Home<br/>
-                        <input type="radio"  id="Hybrid" name="work" value="Hybrid"/>Hybrid<br/>
+                        <input type="radio"  id="office" name="work" value="From Sairme Office"/>From Sairme Office<br/>
+                        <input type="radio"  id="home" name="work" value="From Home"/>From Home<br/>
+                        <input type="radio"  id="hybrid" name="work" value="Hybrid"/>Hybrid<br/>
                     </div>
                     <div className="radioBox" id="box2">
                         <h2>Did you contact covid 19? :(</h2>
-                        <input type="radio"  id="CovidYes" name="Yes" value="Yes"/>Yes<br/>
-                        <input type="radio"  id="CovidNo" name="No" value="No"/>No<br/>
+                        <input type="radio"  id="covidYes" name="Yes" value="Yes"/>Yes<br/>
+                        <input type="radio"  id="covidNo" name="No" value="No"/>No<br/>
                     </div>
                     <div className="radioBox" id="box3">
                         <h2>When?</h2>
-                
+                        {/* <BasicDatePicker /> */}
                     </div>
                     <div className="radioBox" id="box4">
                         <h2>Have you been vaccinated?</h2>
-                        <input type="radio"  id="VaccinadYes" name="Yes" value="Yes"/>Yes<br/>
-                        <input type="radio"  id="VaccinaNo" name="No" value="No"/>No<br/>
+                        <input type="radio"  id="vaccinadYes" name="Yes" value="Yes"/>Yes<br/>
+                        <input type="radio"  id="vaccinaNo" name="No" value="No"/>No<br/>
+                    </div>
+                    <div className="radioBox" id="box5">
+                        <h2>When did you get your last covid vaccine?</h2>
+                        {/* <BasicDatePicker /> */}
                     </div>
                 </div>
             </div>
