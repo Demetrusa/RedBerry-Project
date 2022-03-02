bf468f936017cd0eeeb342095ac4915438be7b0b
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-03-02 21:14:46 +0400

    add About component styles

--- my-app/src/components/common/about/About.js
+++ my-app/src/components/common/about/About.js
@@ -26,10 +26,17 @@ function About () {
             </div>
             <div className="aboutRightBox">
                 <div className="aboutRightBoxHeading">
-                    <h2></h2>
+                    <h2>Redberrian Insights</h2>
                 </div>
                 <div className="AboutRightBoxHeading">
-                    <p></p>
+                    <p>We were soo much fun before the pandemic started! Parties almost every 
+                        weekend and lavish employee birthday celebrations! Unfortunately, 
+                        covid ruined that fun like it did almost everything else in the world. 
+                        But we try our best to zhuzh it up a bit. For example, we host biweekly
+                        Devtalks where our senior and lead developers talk about topics they are passionate about.
+                        Previous topics have included Web3, NFT, Laravel 9, Kubernetes, etc. 
+                        We hold these talks in the office but you can join our Zoom broadcast as well.
+                        Feel free to join either as an attendee or a speaker!</p>
                 </div>
             </div>
         </div>
